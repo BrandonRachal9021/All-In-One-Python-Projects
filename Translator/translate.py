0b600d42c712ea7ce5bc5894dfc262a5c183f15c
Brandon Rachal <brandonrachalxula@gmail.com>
2025-09-21 18:42:04 -0500

    Fixed translator, it can also run different languages.

--- Translator/translate.py
+++ Translator/translate.py
@@ -1,23 +1,26 @@
 from tabulate import tabulate
-from googletrans import Translator
+from deep_translator import GoogleTranslator
 
-class TranslateClass(object):
-    def __init__(self, word, lang):
-        self.word = word
-        self.lang = lang
-        self.Trans = Translator(service_urls=["translate.google.com"])
+class TranslateClass:
+    def __init__(self, text, dest_lang="ar"):
+        self.text = text
+        self.dest_lang = dest_lang
 
-    def __repr__(self):
-        translated = self.Trans.translate(self.word, dest=self.lang).text
+    def translate(self) -> str:
+        return GoogleTranslator(source="auto", target=self.dest_lang).translate(self.text)
+
+    def as_table(self) -> str:
+        translated = self.translate()
         data = [
-            ['Language:', "Sentence"],
-            ['English', self.word],
-            ['Hindi', str(translated)]]
-        table = str(tabulate(data, headers="firstrow", tablefmt="grid"))
-        return table
+            ["Language", "Sentence"],
+            ["English", self.text],
+            ["Hindi", translated],
+        ]
+        return tabulate(data, headers="firstrow", tablefmt="grid")
 
+    def __str__(self):
+        return self.as_table()
 
-if __name__ == '__main__':
-    translate = input('Enter Sentence: ')
-    language = 'hi'
-    print(TranslateClass(translate, language))
+if __name__ == "__main__":
+    sentence = input("Enter Sentence: ")
+    print(TranslateClass(sentence, "ar"))
